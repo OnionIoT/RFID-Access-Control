ba99d7c42a38bbd9a6bf59c8355f3da47478288e
pavelmetrokhin <31224685+pavelmetrokhin@users.noreply.github.com>
2018-04-17 16:28:06 -0400

    Update rfid-access-control.py

--- rfid-access-control.py
+++ rfid-access-control.py
@@ -7,23 +7,23 @@ import json
 #initializing the Relay and Oled Expansions, and block the lock if it's open
 def initial_setup():
         status_oled = oledExp.driverInit()
-        status_relay = relayExp.driverInit(7)
-        check = relayExp.readChannel(7, 0)
+        status_relay = relayExp.driverInit(7)	# 7 is the address of the Relay Expansion; 7 is when all relay switches are switched OFF
+        check = relayExp.readChannel(7, 0)	# (7, 0) - again 7 is the address and 0 is the relay channel
         if check == 1:
                 close_lock()
-        with open('data.json') as json_file:
+        with open('data.json') as json_file:	# Your UIDs could be different, make changes to the data.json file according to your settings
                 data = json.load(json_file)
                 return data['accepted']
         return None
 
 #close the lock
 def close_lock():
-        relayExp.setChannel(7,0,0)
+        relayExp.setChannel(7,0,0)		# Setting Relay with address 7, channel 0 and state is 0 (OFF)
         return
 		
 #open the lock
 def open_lock():
-        relayExp.setChannel(7,0,1)
+        relayExp.setChannel(7,0,1)		# # Setting Relay with address 7, channel 0 and state is 1 (ON)
         return
 
 #function to access the lock for 5 seconds, print the output on the OLED expansion and close the lock
